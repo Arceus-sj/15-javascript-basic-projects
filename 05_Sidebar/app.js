0148e275da2ee99498b93e1d949ee4c4c6a9c08b
Shantanu Jana <omni.7x@gmail.com>
2022-06-04 00:10:00 +0530

    updated app.js

--- 05_Sidebar/app.js
+++ 05_Sidebar/app.js
@@ -3,7 +3,17 @@ const closeBtn = document.querySelector('#close-btn-el');
 const sidebar = document.querySelector('.sidebar-container');
 
 
-toggleBtn.addEventListener('click', function () {
-    sidebar.classList.toggle('show');
-});
+// toggleBtn.addEventListener('click', function () {
+//     sidebar.classList.toggle('show');
+// });
 
+// closeBtn.addEventListener('click', function () {
+//     sidebar.classList.toggle('show');
+// });
+
+
+[toggleBtn, closeBtn].forEach(btn => {
+    btn.addEventListener('click', function () {
+        sidebar.classList.toggle('hide');
+    });
+}) 
