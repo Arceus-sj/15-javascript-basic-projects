746269c1c8bcd9e448d817cacd44e704981bf524
Shantanu Jana <omni.7x@gmail.com>
2022-06-08 22:43:58 +0530

    updated app.js

--- 07_Q&A/app.js
+++ 07_Q&A/app.js
@@ -1,15 +1,24 @@
-
-
-for(let i = 0; i < 3; i++) {
-    let showTextBtn = document.querySelectorAll('.question-btn')[i];
-    let showArticle = document.querySelectorAll('article')[i];
-    let closeBtn = document.querySelectorAll('.close-btn')[i];
+// for(let i = 0; i < 3; i++) {
+//     let showTextBtn = document.querySelectorAll('.question-btn')[i];
+//     let showArticle = document.querySelectorAll('article')[i];
+//     let closeBtn = document.querySelectorAll('.close-btn')[i];
     
-    showTextBtn.addEventListener('click', function () {
-        showArticle.classList.add('show-text');
-    });
+//     showTextBtn.addEventListener('click', function () {
+//         showArticle.classList.add('show-text');
+//     });
 
-    closeBtn.addEventListener('click', function () {
-        showArticle.classList.remove('show-text');
-    })
-}
+//     closeBtn.addEventListener('click', function () {
+//         showArticle.classList.remove('show-text');
+//     })
+// }
+
+const btns = document.querySelectorAll('.question-btn');
+
+const showArticle = document.querySelectorAll('article');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (event) {
+        console.log(event.currentTarget);
+        
+    });
+});
